df7a86d81e823beb001b1590116bf869888a2810
Daniel Siebert <danielsiebert2@web.de>
2016-03-01 23:37:52 +0100

    here

--- Ninja/GameScene.swift
+++ Ninja/GameScene.swift
@@ -9,37 +9,51 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    let lanze = SKSpriteNode(imageNamed: "Lanze")
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
-    }
+        backgroundColor = UIColor(red: 159.0/255.0, green: 201.0/255.0, blue: 244.0/255.0, alpha: 1.0)
+        lanze.position = CGPoint(x: 100, y: 55)
+        lanze.size = CGSize(width: 10, height: 110)
+        addChild(lanze)
+        runAction(SKAction.repeatActionForever(
+            SKAction.sequence([
+                SKAction.runBlock(addBallons),
+                SKAction.waitForDuration(0.5)])))
+        }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
+           }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    
+    func random() -> CGFloat {
+        
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    
+    func random (min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (min-max) + max
+    }
+    
+    
+    func addBallons() {
+        let ballon = SKSpriteNode(imageNamed: "Ballon")
+        ballon.position = CGPoint(x: randomX(), y: frame.size.height - 10)
+        addChild(ballon)
+    }
+    
+    func randomX() -> CGFloat {
+        if (frame.size.width * random(0, max: 1) ) > (frame.size.width-10) {
+            return frame.size.width * random(0, max: 1) - 20 }
+            
+            else { return frame.size.width * random(0, max: 1) }
+        
     }
 }
