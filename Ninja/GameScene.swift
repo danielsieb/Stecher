98ff3eae5b7ffd09d10fb38d498586a497dcc9fc
Daniel Siebert <danielsiebert2@web.de>
2016-03-02 12:40:26 +0100

    soo

--- Ninja/GameScene.swift
+++ Ninja/GameScene.swift
@@ -10,25 +10,34 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    let pike = SKSpriteNode(imageNamed: "Pike")
+    var pike: Pike!
     
     override func didMoveToView(view: SKView) {
         backgroundColor = UIColor(red: 159.0/255.0, green: 201.0/255.0, blue: 244.0/255.0, alpha: 1.0)
-        pike.position = CGPoint(x: 100, y: 55)
-        pike.size = CGSize(width: 10, height: 110)
+        
+        addPike()
+        addBalloons()
+        
+    }
+    
+    func addPike() {
+        pike = Pike()
         addChild(pike)
+    }
+    
+    func addBalloons() {
         runAction(SKAction.repeatActionForever(
             SKAction.sequence([
-                SKAction.runBlock(addBallons),
+                SKAction.runBlock(addBalloon),
                 SKAction.waitForDuration(1.0)])))
-        }
+    }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
-           }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    
+    func addBalloon() {
+        let balloon = SKSpriteNode(imageNamed: "Balloon")
+        balloon.position = CGPoint(x: getRandomX(), y: frame.size.height)
+        balloon.physicsBody = SKPhysicsBody.init(circleOfRadius: 10)
+        addChild(balloon)
     }
     
     func getRandomX() -> CGFloat {
@@ -36,17 +45,16 @@ class GameScene: SKScene {
         let min = 10
         
         let rndValue = CGFloat(min + Int(arc4random()) % (max - min))
-
+        
         return rndValue
     }
     
-    
-    func addBallons() {
-        let balloon = Balloon(positionX: getRandomX(), positionY: frame.size.height - 10)
-        addChild(balloon)
-        if CGPointEqualToPoint(balloon.position, CGPoint(x: balloon.xScale, y: 100)) {
-            balloon.removeFromParent()
-        }
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
     }
     
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+
 }
--- Ninja/Pike.swift
+++ Ninja/Pike.swift
@@ -10,5 +10,21 @@ import Foundation
 import SpriteKit
 
 class Pike: SKSpriteNode {
+
+    init() {
+        let image = SKTexture(imageNamed: "Pike")
+        let size = CGSize(width: 10, height: 110)
+        let position = CGPoint(x: 100, y: 55)
+        super.init(texture: image, color: UIColor.clearColor(), size: size)
+        loadBody(size, position: position)
+    }
     
+    func loadBody(size: CGSize, position: CGPoint) {
+        self.size = size
+        self.position = position
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
